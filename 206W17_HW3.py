f8d9948839e7dab4397fe5720268576403a73f38
David Nguyen <djnguyen@umich.edu>
2017-02-05 15:02:33 -0500

    FINAL

--- 206W17_HW3.py
+++ 206W17_HW3.py
@@ -6,6 +6,7 @@ import re
 ## COMMENT WITH:
 ## Your section day/time: Thursday 3-4 PM
 ## Any names of people you worked with on this assignment: N/A
+## GitHub Repository Link: https://github.com/djnguyen/HW3
 
 #####################
 
