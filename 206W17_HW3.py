f5ddac2cd239484536644f9206e328cd55f58a73
David Nguyen <djnguyen@umich.edu>
2017-02-04 19:40:44 -0500

    part 2-c done

--- 206W17_HW3.py
+++ 206W17_HW3.py
@@ -35,13 +35,19 @@ def parse_counted_words(input_string):
 ## PART 2: 200 points
 
 ## We have provided a text file computer_paths.txt. It's not incredibly long -- you can scan through it, but do NOT hard code your answers! Each line contains 1 filesystem path.
-
+input_file = open("computer_paths.txt", 'r')
+processed_file = input_file.read()
 
 ## (a) Write Python code to determine how many of these paths identify FILES, not directories. Save that number in the variable file_paths_num.
+file_list = re.findall("\.\w+", processed_file)
+file_paths_num = len(file_list)
 
 ## (b) Write Python code to determine how many of these paths are FULL paths, not relative paths. Save that number in the variable full_paths_num.
-
+full_paths_list = re.findall("(\/Users|~)", processed_file)
+full_paths_num = len(full_paths_list)
 ## (c) Write Python code to determine how many of these paths describe a Python file saved inside a folder called SI206. Save that number in the variable python_course_paths.
+python_course_list = re.findall("SI206/[a-zA-Z0-9]*.[A-Za-z0-9]*\.*py", processed_file)
+python_course_paths = len(python_course_list)
 
 ## (d) Write Python code to determine how many of these paths describe a Microsoft file (a file that EITHER ends with .docx OR .xlsx, but nothing else counts) where the file name ends in a digit. Save that total in the variable microsoft_files_num.
 
